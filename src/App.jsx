b7d730272fe3db6d8e36aef5dbea902769307fd1
Avanish Yadav <158454874+Avanish-03@users.noreply.github.com>
2026-03-27 14:34:46 +0530

    feat: add SVG assets for React and Vite, implement Home component with GSAP animation, and set up Tailwind CSS with Vite

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useRef } from 'react'
+import Home from './components/Home/index';
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className="h-screen w-screen bg-black text-white overflow-hidden flex items-center justify-center">
+  
+        <Home />
+    </div>
+  )
+}
+
+export default App
